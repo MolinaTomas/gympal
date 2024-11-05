d5d606aa9a8ae03b11c4e53a228c45584b0ac42f
Tomas Molina <molinatomas011@gmail.com>
2024-11-05 18:03:37 -0300

    Primer Push login test

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>¡Bienvenido GymPal!</h1>
+        <div className="login-container">
+          <form className="login-form">
+            <label className='label-field'>
+              Correo Electrónico
+              <input type="email" name="email" required className="input-field" />
+            </label>
+            <br />
+            <label className='label-field'>
+              Contraseña
+              <input type="password" name="password" required className="input-field" />
+            </label>
+            <br />
+            <button type="submit" className="submit-button">Iniciar Sesión</button>
+          </form>
+        </div>
       </header>
     </div>
   );
